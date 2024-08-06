cca1f8b65ef1c2613f30734cc567b0fe1da0c7ff
balashqedirli <balasqdirov4@gmail.com>
2024-08-06 15:59:21 +0400

    design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,6 +20,7 @@ function App() {
           <Route path=":productId" element={<Product />}></Route>
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginSignup />} />
+          
 
         </Routes>
       </BrowserRouter>
---
+++ frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Footer.css';
+import footer_logo from '../Assets/logo_big.png';
+import instagram_icon from '../Assets/instagram_icon.png';
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <div className="footer-logo">
+        <img src={footer_logo} alt="" />
+        <p>SHOPPER</p>
+      </div>
+      <ul className='footer-links'>
+            <li>Company</li>
+            <li>Products</li>
+            <li>Offices</li>
+            <li>About</li>
+            <li>Contact</li>
+      </ul>
+      <div className="footer-social-icon">
+        <div className="footer-icons-container">
+            <img src={instagram_icon} alt="" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
