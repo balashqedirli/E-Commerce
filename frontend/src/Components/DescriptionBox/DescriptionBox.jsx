1b116165531f6598b39f2d7d91ce644beda92b0c
balashqedirli <balasqdirov4@gmail.com>
2024-11-11 14:52:30 +0400

    responsive design

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+
+        </div>
+        <div className="descriptionbox-description">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero autem ea sequi, sapiente nemo velit porro nostrum aliquam eius placeat quae consequatur, veniam esse quos obcaecati itaque praesentium voluptatum minima excepturi? Tempora quod eligendi cumque, omnis delectus dolorum velit deserunt quidem numquam, consectetur iure officia id. Inventore quos nobis minus obcaecati optio ratione cumque eos, voluptas libero excepturi eum totam, aperiam deleniti placeat officiis maiores fugiat est omnis ab? Magnam sit at laboriosam culpa! Dolor pariatur, quod tempore repudiandae voluptates unde delectus cumque, error distinctio quae, veritatis et? Recusandae, neque!</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, vero ut natus itaque dolore eligendi ullam quibusdam minima sed ex. Vel natus itaque repellendus quam illum alias laborum minima quis.
+          </p>
+        </div>
+      
+    </div>
+  )
+}
+
+export default DescriptionBox
--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './Footer.css';
 import footer_logo from '../Assets/logo_big.png';
 import instagram_icon from '../Assets/instagram_icon.png';
+import pintester_icon from '../Assets/pintester_icon.png';
+import whatsapp_icon from '../Assets/whatsapp_icon.png';
 const Footer = () => {
   return (
     <div className='footer'>
@@ -20,6 +22,16 @@ const Footer = () => {
         <div className="footer-icons-container">
             <img src={instagram_icon} alt="" />
         </div>
+        <div className="footer-icons-container">
+            <img src={pintester_icon} alt="" />
+        </div>
+        <div className="footer-icons-container">
+            <img src={whatsapp_icon} alt="" />
+        </div>
+      </div>
+      <div className="footer-copyright">
+            <hr/>
+            <p>Copyright @ 2024 - All Right Reserved.</p>
       </div>
     </div>
   )
